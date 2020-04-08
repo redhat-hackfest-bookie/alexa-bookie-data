85230cefc26575c4a957451f3580fb8f1e5caf2d
Daniel Vaseekaran <danielvaseekaran@localhost.localdomain>
2020-04-08 18:31:59 -0400

    Commit

---
+++ src/test/java/org/acme/spring/data/jpa/SportResourceTest.java
@@ -0,0 +1,21 @@
+package org.acme.spring.data.jpa;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.IsNot.not;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class SportResourceTest {
+
+    @Test
+    void testListAllSports() {
+
+    }
+
+}
