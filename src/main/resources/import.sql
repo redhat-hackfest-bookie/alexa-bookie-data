eaa60946fb09397d50120afc96fca9c0bb0a6b00
Marcelo Ohashi <mohashi@gmail.com>
2020-06-09 14:12:15 -0300

    Fixing some paths

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,5 +3,5 @@ INSERT INTO sport(id, name, hitCount) VALUES (2, 'cbb', 9);
 INSERT INTO sport(id, name, hitCount) VALUES (3, 'cfb', 3);
 INSERT INTO sport(id, name, hitCount) VALUES (4, 'nba', 4);
 INSERT INTO sport(id, name, hitCount) VALUES (5, 'nhl', 4);
-INSERT INTO sport(id, name, hitCount) VALUES (5, 'mlb', 2);
+INSERT INTO sport(id, name, hitCount) VALUES (6, 'mlb', 2);
 
